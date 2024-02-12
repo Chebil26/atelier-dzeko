6c5c1bb8b5a5ca0659f127370c77d190b931332e
Chebil <aminechebil33@gmail.com>
2024-02-12 22:45:04 +0100

    fix 1

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,12 +5,13 @@ export default function About() {
     <div className="flex items-center justify-center pt-20">
       <div className="w-full max-w-md text-center p-8 bg-amber-50 rounded-lg shadow-lg">
         <p className="text-2xl font-bold mb-4">
-          Découvrez l'élégance intemporelle. ✨ Transformez votre espace avec
-          notre collection unique de meubles et de décoration.
+          Découvrez l&apos;élégance intemporelle. ✨ Transformez votre espace
+          avec notre collection unique de meubles et de décoration.
         </p>
         <p className="text-lg mb-4">
-          Trouvez l'inspiration pour créer un chez-vous qui vous ressemble. 🏡
-          #Atelierdzeko : Votre destination pour le style et le confort.
+          Trouvez l&apos; inspiration pour créer un chez-vous qui vous
+          ressemble. 🏡 #Atelierdzeko : Votre destination pour le style et le
+          confort.
         </p>
       </div>
     </div>

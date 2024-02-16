5ec8eff87914089b221bea4d95555231e5294cc6
Chebil <aminechebil33@gmail.com>
2024-02-16 21:13:53 +0100

    email integration plus prices

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -2,19 +2,18 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend("re_3Fk1ajeG_2jazHguUJH4JDpXJbakKxNgW");
+const resend = new Resend("re_iK5sYtJD_4wGpmMn9RHaEPpxJqXYcTyGp");
 export async function POST(request) {
   try {
     const body = await request.json();
-    console.log(body);
     const { product, price, name, email, phone, address, comment } = body;
 
     const data = await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
-      to: "aminechebil33@gmail.com",
+      from: "Atelier Dzeko <onboarding@resend.dev>",
+      to: "mehdihimeurarchi1@gmail.com",
       subject: `Commande de  ${name} / ${email}`,
       html: `
-      <p><strong>Produut:</strong> ${product} </p>
+      <p><strong>Produit:</strong> ${product} </p>
       <p><strong>Prix:</strong> ${price} </p>
       <p><strong>Name:</strong> ${name} </p>
       <p><strong>Email:</strong> ${email}</p>
